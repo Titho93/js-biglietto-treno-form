15315f80d4162c00eeee09842f4e940f0523bdb0
Titho93 <titho10.93@gmail.com>
2023-09-08 14:59:29 +0200

    [add] const e button

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* 
+Creare un input ed verificarne il funzionamento
+*/
+
+const input1 = document.getElementById('Name');
+const input2 = document.getElementById('Km');
+const input3 = document.getElementById('Age');
+
+const button1 = document.getElementById('btn-1');
+const button2 = document.getElementById('btn-2');
